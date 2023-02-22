f363c6baa2b6d6412b1ab31cfa1a68641ec6c8d3
Kennedy Brito <brito.kennedy10@gmail.com>
2023-02-22 18:06:48 -0300

    componente obter produto

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -24,4 +24,8 @@ showMessage (msg:string) : void {
 create(product: Product): Observable<Product> {
   return this.http.post<Product>(this.baseUrl, product)
 }
+
+read():Observable<Product[]>{
+    return this.http.get<Product[]>(this.baseUrl)
+}
 }
--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -14,7 +14,7 @@ export class ProductCrudComponent implements OnInit{
 
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+   
   }
 
   navigateToProductCreate():void{
